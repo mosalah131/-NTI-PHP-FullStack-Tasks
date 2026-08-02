d66208ec1493ca36fe4b08cd7bc5df5878f4279b
mosalah131 <bnsalah800@gmail.com>
2026-08-02 09:29:02 +0300

    Delete Taskone-Bootstrap-Landingbage directory

--- Taskone-Bootstrap-Landingbage/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const welcomeBtn = document.getElementById('welcomeBtn');
-
-    if (welcomeBtn) {
-        welcomeBtn.addEventListener('click', function() {
-            alert('Welcome! Your custom Bootstrap Landing Page is fully functional. 🚀');
-        });
-    }
-});
